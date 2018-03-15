e135431f08a2c9b66b03acb6660cd530e062aa47
juhojung417512 <jooho2479@gmail.com>
2018-03-15 07:00:30 +0000

    setting

--- web/__init__.py
+++ web/__init__.py
@@ -11,6 +11,11 @@ app = Flask(__name__)
 def home():
 	crawler = parser.Crawler()
 	print(crawler.data)
+	f = codecs.open('templates/home.html','r',encoding='utf-8')
+	if f.readlines() != None : 
+		f.close()
+	        f = codecs.open('templates/home.html','w',encoding='utf-8')
+		f.close()	
 	f = codecs.open('templates/home.html','w',encoding='utf-8')
 	f.write(crawler.html)
 	f.close()
