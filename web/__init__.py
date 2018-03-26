9f6ee0c9d00c406b239d3223ea588f92d5fef3ab
Ubuntu <ubuntu@ip-172-31-34-192.ap-northeast-1.compute.internal>
2018-03-26 18:01:09 +0000

    web crawling 3/4 & logging

--- web/__init__.py
+++ web/__init__.py
@@ -1,15 +1,32 @@
+# default import library
 from flask import Flask , render_template
 import requests
 import codecs
 from bs4 import BeautifulSoup
-from parser import Crawler
+import logging
+from logging.handlers import RotatingFileHandler
+from logging import Formatter
 import pdb
+# module import
+from parser import Crawler
 
 app = Flask(__name__)
+
+log_filepath = '/home/ubuntu/WebService/web/logs'
+
+# logging
+if not app.debug:
+    err_f_handler = RotatingFileHandler('{0}/error.log'.format(log_filepath), maxBytes=1024*1024*1000, backupCount=20)
+    err_f_handler.setLevel(logging.ERROR)
+    err_f_handler.setFormatter(Formatter(
+        '----------------------------------------------------------------------------------\n'
+        '[%(asctime)s] [%(levelname)s]: %(message)s'
+    ))
+    app.logger.addHandler(err_f_handler)
 
 @app.route('/')
 def home():
 	crawler = Crawler()
-	crawler.body_filed_crawling('div','class','company')
-	return render_template('home.html',data = ''.join(crawler.data))
+	data = crawler.body_filed_crawling('div','class','company')
+	return render_template('home.html',data = data)
 
--- web/parser.py
+++ web/parser.py
@@ -5,25 +5,24 @@ import requests
 from bs4 import BeautifulSoup
 import pdb
 from selenium import webdriver 
-from pyvirtualdisplay import Display
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
 class Crawler :
 	def __init__(self):
 		#chrome browser를 열기위한 display 실행
-		display = Display(visible=0, size=(800, 800))  
-		display.start()
 		# setup Driver|Chrome : 크롬드라이버를 사용하는 driver 생성
-		self.driver = webdriver.Chrome('/usr/bin/chromedriver')
+		chrome_options = Options()
+		chrome_options.add_argument("--headless")
+		self.driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=chrome_options)
 		self.driver.implicitly_wait(3)# 암묵적으로 웹 자원을 최대 3초 기다림
 		self.driver.get('http://www.gamejob.co.kr/main/home')# 로그인 페이지 이동
 		self.html = self.driver.page_source
 		self.soup = BeautifulSoup(self.html, 'html.parser')
-		self.data = [] 
 		self.driver.quit()
 	def body_filed_crawling(self,html_type,find_type,value):
-		result = self.soup.find(html_type,{find_type : value})
+		result = self.soup.findAll(html_type,{find_type : value})
 		if result :
-			print(result)
-			self.data.append(result)
+			return result	
 		else :
 			print('cant find')
 	def login_page_crawling(self):
